8bf1e69800fc3f9427486c5503d3a5513bccef83
Nikita <Nikita__99@mail.ru>
2022-08-08 11:13:43 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const configArr = {};
+    const bracketsArr = [];
+    bracketsConfig.forEach(([open, close]) => configArr[open] = close);
+    for (let i = 0; i < str.length; i++) {
+        if (bracketsArr.length) {
+            if (configArr[bracketsArr[bracketsArr.length - 1]] === str[i]) {
+              bracketsArr.pop();
+                continue;
+            }
+        }
+        if (configArr[str[i]]){
+          bracketsArr.push(str[i]);
+            continue;
+        }
+        return false;
+    }
+    return !bracketsArr.length;
 }
